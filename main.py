4d4c32859f88260cd861b4015518d649f4ac8595
Sebastian17mm <64148461+Sebastian17mm@users.noreply.github.com>
2022-08-20 10:52:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project. 
                                       # This will get the root logger since no logger in the configuration has this name.
 
-
 app = FastAPI()
 
 
